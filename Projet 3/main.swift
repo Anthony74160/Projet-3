e6f80d71215870ba7f7283d3e5e45dc18bffd71c
Anthony KUNG <anthonykung@macbook-pro-de-anthony.home>
2018-01-19 04:43:23 +0100

    Initial Commit

---
+++ Projet 3/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Projet 3
+//
+//  Created by Anthony KUNG on 19.01.18.
+//  Copyright © 2018 Anthony KUNG. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
